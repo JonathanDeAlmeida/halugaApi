6963eee987c1e4c9221e0204132abc2251a2d524
Jonathan de Almeida <jonathan88994004@gmail.com>
2021-03-04 23:21:45 -0300

    segundo ajuste

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -4,19 +4,27 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Models\Responsible;
 use App\Models\Time;
+use App\Models\Place;
 
 class TimeController extends Controller
 {
     public function create (Request $request)
     {
-        $time = new Time();
-        $time->place_id = 1;
-        $time->user_id = 1;
-        $time->start = Carbon::now();
-        $time->finish = Carbon::now()->addHour();
-        $time->save();
+        $data = $request->all();
 
-        return $time;
+        // $responsible = Responsible::where('user_id', $data['userId'])->first();
+        // $place = Place::where('responsible_id', $responsible->id)->first();
+
+        // $time = new Time();
+        // $time->place_id = $place->id;
+        // $time->user_id = $data['userId'];
+        // $time->start = Carbon::now();
+        // $time->finish = Carbon::now()->addHour();
+        // $time->save();
+
+        // return $time;
+        return $data;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::post('user-create', 'App\Http\Controllers\UserController@create');
 Route::get('responsible-create', 'App\Http\Controllers\ResponsibleController@create');
 Route::get('message-create', 'App\Http\Controllers\MessageController@create');
 Route::post('place-create', 'App\Http\Controllers\PlaceController@create');
-Route::get('time-create', 'App\Http\Controllers\TimeController@create');
+Route::post('time-create', 'App\Http\Controllers\TimeController@create');
 Route::get('phone-create', 'App\Http\Controllers\PhoneController@create');
 Route::get('adresse-create', 'App\Http\Controllers\AddressController@create');
 
